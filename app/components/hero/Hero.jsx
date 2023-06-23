2b19af535218acb1f9e2f0c01a07f005dc0636e7
HazWahb8080 <hazem.wahb100@gmail.com>
2023-06-23 18:51:28 +0300

    df

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
 
   return (
     <main className="min-h-screen w-full items-center justify-start flex flex-col pt-12">
-      <span className="w-full items-center justify-center flex flex-col space-y-2 text-[#01213A]">
+      <span className="w-full z-50 items-center justify-center flex flex-col space-y-2 text-[#01213A]">
         <h2 className="text-6xl lg:text-[70px] font-bold tracking-tight">
           Track your daily
         </h2>
@@ -28,7 +28,7 @@ function Hero() {
         No more complex codes or boring designs. Creating an <br /> attractive
         and functional site has never been easier.
       </h3>
-      <span className="w-full lg:flex items-center justify-center flex-col-1 lg:space-x-12 space-y-4 lg:space-y-0 mt-6">
+      <span className="w-full z-50 lg:flex items-center justify-center flex-col-1 lg:space-x-12 space-y-4 lg:space-y-0 mt-6">
         <button
           className="bg-[#0044EB] px-6 py-3 rounded-3xl text-white
          overflow-hidden flex smooth hover:bg-black group hover:shadow-2xl hover:shadow-black/80
@@ -45,7 +45,7 @@ function Hero() {
           <ArrowRightIcon className="h-4 w-4 self-center" />
         </button>
         <button
-          className="bg-white border border-black/30 hover:border-transparent px-6 py-3 rounded-3xl text-black
+          className="bg-transparent border border-black/30 hover:border-transparent px-6 py-3 rounded-3xl text-black
          overflow-hidden flex smooth hover:bg-black group hover:shadow-2xl hover:shadow-black/80
           hover:text-white min-w-[180px] max-w-96"
         >
@@ -65,17 +65,26 @@ function Hero() {
           </span>
         </button>
       </span>
-      <motion.img
-        initial={{ scale: 1 }}
-        style={{
-          scale: 1 + scrollProgress * 0.2,
-        }}
-        src="/hero.jpg"
-        width={1000}
-        height={800}
-        alt="hero-img"
-        className="mt-12 object-cover object-center"
-      />
+      <span className="w-full items-center justify-center flex relative">
+        <motion.img
+          initial={{ scale: 1 }}
+          style={{
+            scale: 1 + scrollProgress * 0.2,
+          }}
+          src="/hero.jpg"
+          width={1000}
+          height={800}
+          alt="hero-img"
+          className="mt-12 object-cover object-center z-50"
+        />
+        <Image
+          src="/circles.svg"
+          width={1920}
+          height={1080}
+          alt="hero-circles"
+          className="object-cover object-center absolute z-0 -bottom-48"
+        />
+      </span>
     </main>
   );
 }
