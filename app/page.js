204f305a1a32fb9fcff353dbf6f5c7dc36d3b2bc
HazWahb8080 <hazem.wahb100@gmail.com>
2023-06-28 01:35:34 +0300

    fg

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ function Home() {
   return (
     <main
       className="flex min-h-screen flex-col
-     items-center justify-start"
+     items-center justify-start overflow-x-hidden"
     >
       <TopBanner />
       <section
